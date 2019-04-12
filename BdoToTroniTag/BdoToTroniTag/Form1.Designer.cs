3e7fa77517acc1273502bcca6d5ca831ab6e82f0
Archil Burduli <achiko@hotmail.com>
2019-04-12 16:55:37 +0400

    BDO to Tronitag

---
+++ BdoToTroniTag/BdoToTroniTag/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BdoToTroniTag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export to TroniTAG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 149);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(397, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFileName.Location = new System.Drawing.Point(28, 81);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.ReadOnly = true;
+            this.tbFileName.Size = new System.Drawing.Size(345, 20);
+            this.tbFileName.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(28, 108);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Upload By FTP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 171);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BDO To TroniTAG";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.TextBox tbFileName;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ BdoToTroniTag/BdoToTroniTag/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BdoToTroniTag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<string> res = new List<string>();
+            res.Add(Properties.Settings.Default.HEADER);
+            statusStrip1.Items[0].Text = "Get BDO Records...";
+            Application.DoEvents();
+            BalanceLib.BalanceConnector conn = new BalanceLib.BalanceConnector(Properties.Settings.Default.BDOUrl,
+                                Properties.Settings.Default.BDOUser,
+                                Properties.Settings.Default.BDOPass);
+
+            try
+            { 
+                var items  = conn.GetItems();
+                foreach (var p in items.Where(x=>!x.IsGroup))
+                {
+                    //if (Properties.Settings.Default.BDOGroupFilter != "" && p.Group == new Guid(Properties.Settings.Default.BDOGroupFilter))
+                    if (!string.IsNullOrEmpty( p.Barcode))
+                    {
+                        string price = p.Price.Replace(",", ".");
+                        if (Properties.Settings.Default.PriceDevider > 1)
+                        {
+                            decimal lpr = 0;
+                            try
+                            {
+                                lpr = decimal.Parse(price);
+                                lpr = lpr / Properties.Settings.Default.PriceDevider;
+                            }
+                            catch
+                            {
+                                lpr = 0;
+                            }
+                            if (lpr > 0)
+                            {
+                                price = lpr.ToString("0.##");
+                            }
+
+
+                        }
+
+                        string oldPrice = "";
+                        string isPromo = "";
+                        if (!string.IsNullOrEmpty(p.AdditionalRequisites1))
+                        {
+
+                            decimal lpr = 0;
+                            try
+                            {
+                                lpr = decimal.Parse(p.AdditionalRequisites1.Replace(",", "."));
+                                if (p.Unit != "ცალი")
+                                    lpr = lpr / Properties.Settings.Default.PriceDevider;
+                            }
+                            catch
+                            {
+                                lpr = 0;
+                            }
+                            if (lpr > 0)
+                            {
+                                oldPrice = lpr.ToString("0.##");
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(p.AdditionalRequisites2))
+                        {
+                            try
+                            {
+                                isPromo = p.AdditionalRequisites2.ToUpper() == "TRUE" ? "promo" : "";
+                            }
+                            catch { }
+                        }
+
+                        string szUnit = "100 გრ.";
+                        if ( (p.Barcode.Length>=8 && !p.Barcode.StartsWith("22")) || p.Unit== "ცალი" )
+                        {
+                            szUnit = ""; 
+                            price = p.Price.Replace(",", ".");
+                        }
+
+                        string enName = "";
+                        if (string.IsNullOrEmpty(p.AdditionalRequisites3))
+                        {
+                            enName = p.FullName.Replace(",", " ");
+                        }
+                        else
+                        {
+                            enName = p.AdditionalRequisites3.Replace(",", " ");
+                        }
+                        string ns = $"{p.Barcode.Replace(",", " ")},{p.Name.Replace(","," ")},{enName},{price.Replace(",", ".")},₾,{isPromo},{szUnit.Replace(",", ".")},{oldPrice.Replace(",", " ")}";
+                        //string ns = $"\"{p.Barcode}\",\"{p.Name.Replace(",", " ")}\",\"{p.FullName.Replace(",", " ")}\",\"{price.Replace(",", ".")}\",\"₾\",\"{isPromo}\",\"{szUnit.Replace(",", ".")}\",\"{oldPrice}\"";
+                        res.Add(ns);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                statusStrip1.Items[0].Text = ex.Message;
+                return;
+            }
+
+            statusStrip1.Items[0].Text = "Writing file...";
+            Application.DoEvents();
+
+            string path = Path.GetTempPath();
+            DateTime d = DateTime.Now;
+            //string fname = $"import_{d.Year}{d.Month}{d.Day}{d.Hour}{d.Minute}{d.Second}.csv";
+            string fname = string.Format("import_{0:0000}{1:00}{2:00}{3:00}{4:00}{5:00}.csv",d.Year,d.Month,d.Day,d.Hour,d.Minute,d.Second); 
+            string fullfilename = Path.Combine(path, fname);
+            tbFileName.Text = fullfilename;
+            StreamWriter sw = new StreamWriter(fullfilename,false,Encoding.Unicode);
+            foreach (var i in res)
+            {
+                sw.WriteLine(i);
+            }
+            sw.Close();
+
+            if (checkBox1.Checked)
+            {
+                statusStrip1.Items[0].Text = $"Uploading To FTP: {Properties.Settings.Default.OutputFTPUrl}";
+                Application.DoEvents();
+
+                string url = $"{Properties.Settings.Default.OutputFTPUrl}/{Properties.Settings.Default.Folder}";
+                Upload(url, Properties.Settings.Default.FTPUser,
+                    Properties.Settings.Default.FTPPass, fullfilename);
+            }
+            else
+            {
+                File.Copy(fullfilename, Path.Combine(Properties.Settings.Default.DirectFilePath, fname));
+            }
+            statusStrip1.Items[0].Text = "Finish";
+
+
+
+
+
+
+
+
+        }
+
+        private void Upload(string ftpServer, string userName, string password, string filename)
+        {
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                client.Credentials = new System.Net.NetworkCredential(userName, password);
+                client.UploadFile(ftpServer + "/" + new FileInfo(filename).Name, "STOR", filename);
+            }
+        }
+
+        
+    }
+}
